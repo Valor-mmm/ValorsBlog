013d993b0f245599c852f35acff6d32aad7a51b1
Valor-mmm <v_dev/github@slmail.me>
2025-12-28 23:08:09 +0100

    Fix failing tests

--- components/about/Education_test.tsx
+++ components/about/Education_test.tsx
@@ -6,7 +6,8 @@ Deno.test("Education component renders correctly", () => {
   const html = render(<Education />);
   assertEquals(html.includes("Education"), true);
   assertEquals(html.includes("B.Sc. Information Technology"), true);
-  assertEquals(html.includes("M.Sc. IT Focus Software Engineering"), true);
+  assertEquals(html.includes("M.Sc. Information Technology"), true);
+  assertEquals(html.includes("Focus on Software Engineering"), true);
   assertEquals(html.includes("1.9"), true);
   assertEquals(html.includes("2014"), true);
   assertEquals(html.includes("2018"), true);
